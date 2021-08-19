8ae2cbc70fdfa2ad3f5481cbdc34ff75b3b8f850
ummaeha <raya000@naver.com>
2021-08-19 18:14:05 +0900

    redux 셀프스터디중

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import history from "./history";
 
 import Main from "./pages/Main";
 import Three from "./pages/Three";
+import './App.css'
+import Subscribers from "./components/Subscribers";
+import View from "./components/View"
+import Display from "./components/Display";
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
     <div className="App">
+      <Provider store={store}>
+        <Subscribers />
+        <View />
+        <Display />
+      </Provider>
       <Router history={history}>
         <Switch>
           <div>
             <Route path="/" exact component={Main}/>
             <Route path="/three" exact component={Three}/>
+            {/* <Route path="/redux" component={Subscribers}></Route> */}
           </div>
         </Switch>
       </Router>
